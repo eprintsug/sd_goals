86232533b7b5b2c2e610f00e8527c49aeac748fb
Eleanor Dumbill <eleanor.dumbill@cosector.com>
2023-10-30 15:37:55 +0000

    Initial SDG commit

---
+++ cfg/cfg.d/zzz_sdg.pl
@@ -0,0 +1,25 @@
+push @{$c->{fields}->{eprint}},
+{
+    name => 'sd_goals',
+    type => 'namedset',
+    set_name => 'sd_goals',
+    multiple => 1,
+};
+
+push @{$c->{browse_views}},
+{
+    id => "sd_goals",
+    menus => [
+        {
+            fields => [ "sd_goals" ],
+            allow_null => 0,
+        },
+    ],
+    order => "creators_browse_name/title",
+    variations => [
+        "date;truncate=4,reverse",
+        "creators_browse_name;first_letter",
+        "type",
+        "DEFAULT",
+    ],
+};
